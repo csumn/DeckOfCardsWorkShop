79bdb6c18097a15bba71ea5e36d19017f2fcc083
csumn <csumn13@gmail.com>
2022-03-28 10:09:38 +0530

    modified slightly

--- DeckOfCards/src/com/bridgelabz/Main.java
+++ DeckOfCards/src/com/bridgelabz/Main.java
@@ -1,9 +1,11 @@
 package com.bridgelabz;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) {
+		System.out.println("---Welcome to Deck Of Cards Game---");
 		DeckOfCards deckOfCards = new DeckOfCards();
-		deckOfCards.welcome();
-		deckOfCards.deckOfCards();
+		deckOfCards.getUniqueCards();
+		System.out.println("****Cards are****");
+		deckOfCards.displayArray(DeckOfCards.deckOfCard);
 	}
 }
