2bdc644fbd153ec5f8da180c73ed2f1d5ca3baa9
csumn <csumn13@gmail.com>
2022-03-28 10:48:35 +0530

    created player class

--- DeckOfCards/src/com/bridgelabz/DeckOfCards.java
+++ DeckOfCards/src/com/bridgelabz/DeckOfCards.java
@@ -1,33 +1,46 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class DeckOfCards {
+public class DeckOfCards{
+	static String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	static String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+	static String[] deckOfCard = new String[suit.length * rank.length];
+	public static ArrayList<Player> playerList = new ArrayList<>();
 
-	public ArrayList<String> cardsDeck = new ArrayList<>();
-
-	public void welcome(){
-		System.out.println("Welcome Deck of Cards game");
-	}
-
-	public void deckOfCards(){
-		String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
-		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
-		int numOfCards = suits.length * ranks.length;
-		System.out.println("\nNumber of cards in the deck : " + numOfCards);
-		for (int i = 0; i < ranks.length; i++) {
-			for (int j = 0; j < suits.length; j++) {
-				cardsDeck.add(ranks[i] + "--->" + suits[j]);
+	Scanner scanner =  new Scanner(System.in);
+	public void getUniqueCards() {
+		int index = 0;
+		for (int i = 0; i < suit.length; i++) {
+			for (int j = 0; j < rank.length; j++) {
+				deckOfCard[index] = suit[i] + " : " + rank[j];
+				index++;
 			}
 		}
-		toDisplay(cardsDeck);
 	}
-
-	public static void toDisplay(ArrayList<String> cardsDeck) {
-		System.out.println("\nCards in Deck : ");
-		for (String element : cardsDeck) {
-			System.out.println(element);
+	public void displayArray(String[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
 		}
 		System.out.println();
+	}
+
+	public void numOfPlayers() {
+		System.out.println("Enter the number of players must be between 2 to 4 ");
+		int playerCount = scanner.nextInt();
+		if (playerCount >= 2 && playerCount <= 4) {
+
+			for(int i =0;i<playerCount;i++){
+				System.out.println("Enter player "+(i+1)+" name");
+				String name = scanner.next();
+				int playerNo = (i+1);
+				playerList.add(new Player(name,playerNo));
+			}
+			System.out.println(playerCount + " players will play the game");
+		} else {
+			System.out.println("Invalid Number of Players .....");
+			numOfPlayers();
+		}
 	}
 }
--- DeckOfCards/src/com/bridgelabz/Main.java
+++ DeckOfCards/src/com/bridgelabz/Main.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) {
+		System.out.println("---Welcome to Deck Of Cards Game---");
+		
 		DeckOfCards deckOfCards = new DeckOfCards();
-		deckOfCards.welcome();
-		deckOfCards.deckOfCards();
+		deckOfCards.getUniqueCards();
+		System.out.println("****Cards are****");
+		deckOfCards.displayArray(DeckOfCards.deckOfCard);
+		deckOfCards.numOfPlayers();
+		System.out.println(DeckOfCards.playerList);
 	}
 }
